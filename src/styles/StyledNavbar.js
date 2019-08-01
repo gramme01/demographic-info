7c57415f6e33e719613f261643fb0a6411d796fa
Emmanuel <gramme.oe@gmail.com>
2019-08-01 04:21:03 +0100

    Navbar

--- src/styles/StyledNavbar.js
+++ src/styles/StyledNavbar.js
@@ -23,8 +23,8 @@ const StyledHeader = styled.header`
 
 	.toggle {
 		position: relative;
-		width: 1.2rem;
-		height: 0.7rem;
+		width: 1.8rem;
+		height: 1.05rem;
 		margin: 0.4rem;
 	}
 
@@ -43,7 +43,7 @@ const StyledHeader = styled.header`
 		left: 0;
 		right: 0;
 		bottom: 0;
-		border-radius: 2rem;
+		border-radius: 3rem;
 		background-color: ${props => props.theme.text};
 		-webkit-transition: 0.4s;
 		transition: 0.4s;
@@ -52,10 +52,10 @@ const StyledHeader = styled.header`
 	.slider:before {
 		position: absolute;
 		content: "";
-		height: 0.5rem;
-		width: 0.5rem;
-		left: 0.1rem;
-		bottom: 0.1rem;
+		height: 0.75rem;
+		width: 0.75rem;
+		left: 0.15rem;
+		bottom: 0.15rem;
 		border-radius: 50%;
 		background-color: ${props => props.theme.background};
 		-webkit-transition: 0.4s;
@@ -67,14 +67,63 @@ const StyledHeader = styled.header`
 	}
 
 	input:checked + .slider:before {
-		-webkit-transform: translateX(0.5rem);
-		-ms-transform: translateX(0.5rem);
-		transform: translateX(0.5rem);
+		-webkit-transform: translateX(0.75rem);
+		-ms-transform: translateX(0.75rem);
+		transform: translateX(0.75rem);
 	}
 
-	@media screen and (min-width: 769px) {
+	/* @media screen and (min-width: 769px) {
 		background: #000;
-	}
+
+		.toggle {
+			position: relative;
+			width: 1.8rem;
+			height: 1.05rem;
+			margin: 0.4rem;
+		}
+
+		.toggle input {
+			opacity: 0;
+			width: 0;
+			height: 0;
+		}
+
+		.slider {
+			position: absolute;
+			cursor: pointer;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			border-radius: 4rem;
+			background-color: ${props => props.theme.text};
+			-webkit-transition: 0.4s;
+			transition: 0.4s;
+		}
+
+		.slider:before {
+			position: absolute;
+			content: "";
+			height: 1rem;
+			width: 1rem;
+			left: 0.2rem;
+			bottom: 0.2rem;
+			border-radius: 50%;
+			background-color: ${props => props.theme.background};
+			-webkit-transition: 0.4s;
+			transition: 0.4s;
+		}
+
+		input:focus + .slider {
+			box-shadow: 0 0 1px #2196f3;
+		}
+
+		input:checked + .slider:before {
+			-webkit-transform: translateX(1rem);
+			-ms-transform: translateX(1rem);
+			transform: translateX(1rem);
+		}
+	} */
 `;
 
 export default StyledHeader;
